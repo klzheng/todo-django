cfe27656c86f9ab5bb48d4b4b8ab98e6e0cd4903
Kevin <klzheng@buffalo.edu>
2023-01-08 21:02:17 -0500

    Separated some react components into their own files Implemented basic bootstrap design on homepage Added key change and form submit functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,83 @@
-import './App.css';
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useState, useEffect, useCallback } from "react";
+import Background from "./components/Background";
+import Container from "./components/Container";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="background">
-      
-    </div>
-  );
+    const [todoList, SetTodoList] = useState([]);
+    const [editing, SetEditing] = useState(false);
+    const [activeItem, SetActiveItem] = useState({
+        id: null,
+        title: "",
+        completed: false,
+    });
+
+    const fetchTasks = useCallback(() => {
+        console.log("Fetching...");
+
+        fetch("http://127.0.0.1:8000/api/todo-list/")
+            .then((response) => response.json())
+            .then((data) => {
+                SetTodoList(data);
+                console.log(data);
+            });
+    }, []);
+
+    const handleChange = (e) => {
+        SetActiveItem({
+            ...activeItem,
+            title: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(activeItem);
+        fetch("http://127.0.0.1:8000/api/todo-create/", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(activeItem),
+        }).then(() => {
+            fetchTasks();
+            SetActiveItem({
+                id: null,
+                title: "",
+                completed: false,
+            })
+        }).catch((err) => {
+            console.log("ERROR:", err)
+        })
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, [fetchTasks]);
+
+    return (
+        <Background>
+            <Container>
+                <Form onSubmit={handleSubmit} className="form-container">
+                    <Row>
+                        <Col>
+                            <Form.Control
+                                onChange={handleChange}
+                                type="textarea"
+                                placeholder="Add task..."
+                            />
+                        </Col>
+                        <Col xs="auto">
+                            <Button variant="primary" type="submit">
+                                Create Item
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Container>
+        </Background>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Background.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Background({ children }) {
+    return (
+        <div className="background">
+            {children}
+        </div>
+    )
+}
